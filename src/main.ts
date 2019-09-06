e1098a2e4a0de845c62ca26cbb65e22fac9329c1
JJ Merelo <jjmerelo@gmail.com>
2019-09-06 12:36:15 +0200

    Updates with real checks

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,10 @@ const parse = require('parse-diff')
 async function run() {
   try {
       const bodyContains = core.getInput('bodyContains')
+      if ( context.payload.pull_request.indexOf( bodyContains) < 0  ) {
+          core.setFailed("The body of the PR does not contain ${bodyContains}");
+      }
+      const diffContains = core.getInput('diffContains')
       const token = core.getInput('github-token', {required: true})
       const github = new GitHub(token, {} )
       const diff_url = context.payload.pull_request.diff_url
