92a9e7387b0958504e3637ff12c8e30bcc41bed7
JJ Merelo <jjmerelo@gmail.com>
2019-09-06 12:03:00 +0200

    Changes to caps

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
 import * as core from '@actions/core';
-const {github, context} = require('@actions/github')
+const {GitHub, context} = require('@actions/github')
 
 async function run() {
   try {
       const bodyContains = core.getInput('bodyContains')
       const token = core.getInput('github-token', {required: true})
-      const client = new github(token, {} )
+      const client = new GitHub(token, {} )
       console.log( context )
       console.log( context.payload )
       console.log(`Body needs to contain ${bodyContains}`)
