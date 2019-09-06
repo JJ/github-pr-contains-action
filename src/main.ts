5b11c176d57459726b303bb42f677b1abbc2dbbf
JJ Merelo <jjmerelo@gmail.com>
2019-09-06 11:51:08 +0200

    Adds token

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,11 @@ import * as github from '@actions/github';
 
 async function run() {
   try {
-    const bodyContains = core.getInput('bodyContains');
-    console.log(`Body needs to contain ${bodyContains}`);
+      const bodyContains = core.getInput('bodyContains');
+      const token = core.getInput('github-token', {required: true})
+      console.log(`Body needs to contain ${bodyContains}`);
   } catch (error) {
-    core.setFailed(error.message);
+      core.setFailed(error.message);
   }
 }
 
