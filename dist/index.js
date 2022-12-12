d20d426cab795003cbc8989f7a40aabcb3c17501
Francesco Giordano <giordafr@amazon.it>
2022-12-12 15:32:11 +0100

    Added main logic

--- dist/index.js
+++ dist/index.js
@@ -35,17 +35,102 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
     });
 };
 Object.defineProperty(exports, "__esModule", ({ value: true }));
+const github = __nccwpck_require__(716);
+const parse = __nccwpck_require__(202);
 const core = __importStar(__nccwpck_require__(186));
-const wait_1 = __nccwpck_require__(817);
 function run() {
     return __awaiter(this, void 0, void 0, function* () {
         try {
-            const ms = core.getInput('milliseconds');
-            core.debug(`Waiting ${ms} milliseconds ...`); // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
-            core.debug(new Date().toTimeString());
-            yield (0, wait_1.wait)(parseInt(ms, 10));
-            core.debug(new Date().toTimeString());
-            core.setOutput('time', new Date().toTimeString());
+            core.debug("Get github token");
+            // get information on everything
+            const token = core.getInput('github-token', { required: true });
+            const octokit = github.getOctokit(token);
+            const context = github.context;
+            core.debug("Check body contains");
+            // Check that the pull request description contains the required string
+            const bodyContains = core.getInput('bodyContains');
+            if (typeof bodyContains !== 'undefined') {
+                if (bodyContains && !context.payload.pull_request.body.includes(bodyContains)) {
+                    core.setFailed("The PR description should include " + bodyContains);
+                }
+            }
+            core.debug("Check body does not contain");
+            // Check that the pull request description does not contain the forbidden string
+            const bodyDoesNotContain = core.getInput('bodyDoesNotContain');
+            if (typeof bodyDoesNotContain !== 'undefined') {
+                if (bodyDoesNotContain && context.payload.pull_request.body.includes(bodyDoesNotContain)) {
+                    core.setFailed("The PR description should not include " + bodyDoesNotContain);
+                }
+            }
+            core.debug("Get diff from github PR");
+            // Request the pull request diff from the GitHub API
+            const { data: prDiff } = yield octokit.pulls.get({
+                owner: context.repo.owner,
+                repo: context.repo.repo,
+                pull_number: context.payload.pull_request.number,
+                mediaType: {
+                    format: "diff",
+                },
+            });
+            const files = parse(prDiff);
+            core.debug("Check max files changed");
+            // Check that no more than the specified number of files were changed
+            const maxFilesChanged = core.getInput('maxFilesChanged');
+            if (typeof maxFilesChanged !== 'undefined') {
+                if (maxFilesChanged && files.length > maxFilesChanged) {
+                    core.setFailed("The PR shouldn not change more than " + maxFilesChanged + " file(s)");
+                }
+            }
+            // Get changed chunks
+            var changes = '';
+            var additions = 0;
+            files.forEach(function (file) {
+                additions += file.additions;
+                file.chunks.forEach(function (chunk) {
+                    chunk.changes.forEach(function (change) {
+                        if (change.add) {
+                            changes += change.content;
+                        }
+                    });
+                });
+            });
+            //core.debug('pabbelt:', changes)
+            core.debug("Check max lines changed");
+            // Check that no more than the specified number of lines have changed
+            const maxLinesChanged = parseInt(core.getInput('maxLinesChanged'));
+            if (typeof maxLinesChanged !== 'undefined') {
+                if (maxLinesChanged && (additions > maxLinesChanged)) {
+                    core.setFailed("The PR shouldn not change more than " + maxLinesChanged + " lines(s) ");
+                }
+            }
+            // Check that the pull request diff constains the required string
+            const diffContains = core.getInput('diffContains');
+            if (typeof diffContains !== 'undefined') {
+                if (diffContains && !changes.includes(diffContains)) {
+                    core.setFailed("The PR diff should include " + diffContains);
+                }
+            }
+            // Check that the pull request diff does not contain the forbidden string
+            const diffDoesNotContain = core.getInput('diffDoesNotContain');
+            if (typeof diffDoesNotContain !== 'undefined') {
+                if (diffDoesNotContain && changes.includes(diffDoesNotContain)) {
+                    core.setFailed("The PR diff should not include " + diffDoesNotContain);
+                }
+            }
+            // Check that the pull request diff constains the required regex
+            const diffContainsRegex = core.getInput('diffContainsRegex');
+            if (typeof diffContainsRegex !== 'undefined') {
+                if (diffContains && !RegExp(diffContainsRegex).test(changes)) {
+                    core.setFailed("The PR diff should include a string matching" + diffContainsRegex);
+                }
+            }
+            // Check that the pull request diff does not contain the forbidden regex
+            const diffDoesNotContainRegex = core.getInput('diffDoesNotContainRegex');
+            if (typeof diffDoesNotContainRegex !== 'undefined') {
+                if (diffDoesNotContain && RegExp(diffDoesNotContainRegex).test(changes)) {
+                    core.setFailed("The PR diff should not include a string matching" + diffDoesNotContainRegex);
+                }
+            }
         }
         catch (error) {
             if (error instanceof Error)
@@ -54,37 +139,6 @@ function run() {
     });
 }
 run();
-
-
-/***/ }),
-
-/***/ 817:
-/***/ (function(__unused_webpack_module, exports) {
-
-"use strict";
-
-var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
-    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
-    return new (P || (P = Promise))(function (resolve, reject) {
-        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
-        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
-        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
-        step((generator = generator.apply(thisArg, _arguments || [])).next());
-    });
-};
-Object.defineProperty(exports, "__esModule", ({ value: true }));
-exports.wait = void 0;
-function wait(milliseconds) {
-    return __awaiter(this, void 0, void 0, function* () {
-        return new Promise(resolve => {
-            if (isNaN(milliseconds)) {
-                throw new Error('milliseconds not a number');
-            }
-            setTimeout(() => resolve('done!'), milliseconds);
-        });
-    });
-}
-exports.wait = wait;
 
 
 /***/ }),
@@ -2773,6 +2827,22 @@ function version(uuid) {
 
 var _default = version;
 exports.default = _default;
+
+/***/ }),
+
+/***/ 716:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ }),
+
+/***/ 202:
+/***/ ((module) => {
+
+module.exports = eval("require")("parse-diff");
+
 
 /***/ }),
 
--- src/main.ts
+++ src/main.ts
@@ -1,16 +1,113 @@
+const github = require('@actions/github')
+const parse = require('parse-diff')
+
 import * as core from '@actions/core'
 import {wait} from './wait'
 
 async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_STEP_DEBUG` to true
+    core.debug("Get github token")
+    // get information on everything
+    const token = core.getInput('github-token', { required: true })
+    const octokit = github.getOctokit(token)
+    const context = github.context
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.debug("Check body contains")
+    // Check that the pull request description contains the required string
+    const bodyContains = core.getInput('bodyContains')
+    if (typeof bodyContains !== 'undefined') {
+      if (bodyContains && !context.payload.pull_request.body.includes(bodyContains)) {
+        core.setFailed("The PR description should include " + bodyContains)
+      }
+    }
 
-    core.setOutput('time', new Date().toTimeString())
+    core.debug("Check body does not contain")
+    // Check that the pull request description does not contain the forbidden string
+    const bodyDoesNotContain = core.getInput('bodyDoesNotContain')
+    if (typeof bodyDoesNotContain !== 'undefined') {
+      if (bodyDoesNotContain && context.payload.pull_request.body.includes(bodyDoesNotContain)) {
+        core.setFailed("The PR description should not include " + bodyDoesNotContain);
+      }
+    }
+
+    core.debug("Get diff from github PR")
+    // Request the pull request diff from the GitHub API
+    const { data: prDiff } = await octokit.pulls.get({
+      owner: context.repo.owner,
+      repo: context.repo.repo,
+      pull_number: context.payload.pull_request.number,
+      mediaType: {
+        format: "diff",
+      },
+    });
+    const files = parse(prDiff)
+
+    core.debug("Check max files changed")
+    // Check that no more than the specified number of files were changed
+    const maxFilesChanged = core.getInput('maxFilesChanged')
+    if (typeof maxFilesChanged !== 'undefined') {
+      if (maxFilesChanged && files.length > maxFilesChanged) {
+        core.setFailed("The PR shouldn not change more than " + maxFilesChanged + " file(s)");
+      }
+    }
+
+    // Get changed chunks
+    var changes = ''
+    var additions: number = 0
+    files.forEach(function (file: any) {
+      additions += file.additions
+      file.chunks.forEach(function (chunk: any) {
+        chunk.changes.forEach(function (change: any) {
+          if (change.add) {
+            changes += change.content
+          }
+        })
+      })
+    })
+    
+    //core.debug('pabbelt:', changes)
+
+    core.debug("Check max lines changed")
+    // Check that no more than the specified number of lines have changed
+    const maxLinesChanged = parseInt(core.getInput('maxLinesChanged'))
+    if (typeof maxLinesChanged !== 'undefined') {
+      if (maxLinesChanged && (additions > maxLinesChanged)) {
+        core.setFailed("The PR shouldn not change more than " + maxLinesChanged + " lines(s) ");
+      }
+    }
+
+    // Check that the pull request diff constains the required string
+    const diffContains = core.getInput('diffContains')
+    if (typeof diffContains !== 'undefined') {
+      if (diffContains && !changes.includes(diffContains)) {
+        core.setFailed("The PR diff should include " + diffContains);
+      }
+    }
+
+    // Check that the pull request diff does not contain the forbidden string
+    const diffDoesNotContain = core.getInput('diffDoesNotContain')
+    if (typeof diffDoesNotContain !== 'undefined') {
+      if (diffDoesNotContain && changes.includes(diffDoesNotContain)) {
+        core.setFailed("The PR diff should not include " + diffDoesNotContain);
+      }
+    }
+
+    // Check that the pull request diff constains the required regex
+    const diffContainsRegex = core.getInput('diffContainsRegex')
+    if (typeof diffContainsRegex !== 'undefined') {
+      if (diffContains && !RegExp(diffContainsRegex).test(changes)) {
+        core.setFailed("The PR diff should include a string matching" + diffContainsRegex);
+      }
+    }
+
+    // Check that the pull request diff does not contain the forbidden regex
+    const diffDoesNotContainRegex = core.getInput('diffDoesNotContainRegex')
+    if (typeof diffDoesNotContainRegex !== 'undefined') {
+      if (diffDoesNotContain && RegExp(diffDoesNotContainRegex).test(changes)) {
+        core.setFailed("The PR diff should not include a string matching" + diffDoesNotContainRegex);
+      }
+    }
+
   } catch (error) {
     if (error instanceof Error) core.setFailed(error.message)
   }
