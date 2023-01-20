10854cca19382ecc585a90fc73ca2b65f7e5249d
JJ Merelo <jjmerelo@gmail.com>
2023-01-20 17:20:42 +0100

    :white_check_mark: #32

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -5,4 +5,10 @@ describe("Regex creator", () => {
     const re1: RegExp = rexify("foo");
     expect("foobar").toMatch(re1);
   });
+
+  it("Should create multi-string regexes", () => {
+    const re1: RegExp = rexify(["foo", "bar", "baz"]);
+    expect("foobar").toMatch(re1);
+    expect("1234").not.toMatch(re1);
+  });
 });
