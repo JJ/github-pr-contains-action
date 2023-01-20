c699b7aeedd548b23112c09ffca4c3bd0298cd86
JJ Merelo <jjmerelo@gmail.com>
2023-01-20 14:22:45 +0100

    :white_check-mark: :recycle: #32
    Spinning off utility functions, and adding tests

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,1 +1,8 @@
-import { rexify } from "../src/utils.ts";
+import { rexify } from "../src/utils";
+
+describe("Regex creator", () => {
+  it("Should create single-string regexes", () => {
+    const re1: RegExp = rexify("foo");
+    expect("foobar").toMatch(re1);
+  });
+});
