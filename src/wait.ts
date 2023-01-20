69419adc05dad5a21bf307e0868cde4b56f665ee
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2023-01-20 09:09:21 +0100

    Minor :recycle:
    Simple state the value, instead of the variable name (which is probably not known by the client)

--- src/wait.ts
+++ src/wait.ts
@@ -1,7 +1,7 @@
 export async function wait(milliseconds: number): Promise<string> {
   return new Promise(resolve => {
     if (isNaN(milliseconds)) {
-      throw new Error('milliseconds not a number')
+      throw new Error(`${milliseconds} does not look like a number`)
     }
 
     setTimeout(() => resolve('done!'), milliseconds)
