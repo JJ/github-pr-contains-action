2a556eb27e9076fe2efff8101d2f52e223bcca1b
JJ Merelo <jjmerelo@gmail.com>
2023-01-20 20:45:10 +0100

    Change to single string

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,3 @@
-export function rexify(stringOrArray: string | string[]): RegExp {
-  if (typeof stringOrArray === "string") {
-    return new RegExp(stringOrArray);
-  } else {
-    return new RegExp(stringOrArray.join("|"));
-  }
+export function rexify(stringOrArray: string): RegExp {
+  return new RegExp(stringOrArray);
 }
