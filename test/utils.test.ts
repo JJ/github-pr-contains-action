ec618beefca4ec5dd3e4d0cb348a3bb1f8f0bf5c
Juan Julián Merelo Guervós <jjmerelo@gmail.com>
2023-01-21 11:48:15 +0100

    :construction:

--- test/utils.test.ts
+++ test/utils.test.ts
@@ -1,5 +1,6 @@
 import { rexify } from "../src/utils";
 
+//TODO: stuff
 describe("Regex creator", () => {
   it("Should create single-string regexes", () => {
     const re1: RegExp = rexify("foo");
