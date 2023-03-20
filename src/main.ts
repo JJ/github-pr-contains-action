0ae47c899b1e75c9a11e007cc4a4265f20214717
JJ Merelo <jjmerelo@gmail.com>
2023-03-20 07:55:37 +0100

    core.setFailed does not exit...

--- src/main.ts
+++ src/main.ts
@@ -20,82 +20,85 @@ async function run() {
       core.setFailed(
         "⚠️ I am sorry, this workflow only works in pull requests"
       );
-    }
-    if (bodyContains || bodyDoesNotContain) {
-      core.info("Checking body contents");
-      if (!context.payload.pull_request.hasOwnProperty("body")) {
-        core.setFailed("There's no body in the PR, can't check");
-      } else if (context.payload.pull_request.body === "") {
-        core.setFailed("The body is empty, can't check");
-      } else {
-        const PRBody = context.payload.pull_request.body;
-        if (bodyContains && !rexify(bodyContains).test(PRBody)) {
-          core.setFailed("The body of the PR does not contain " + bodyContains);
+    } else {
+      if (bodyContains || bodyDoesNotContain) {
+        core.info("Checking body contents");
+        if (!context.payload.pull_request.hasOwnProperty("body")) {
+          core.setFailed("There's no body in the PR, can't check");
+        } else if (context.payload.pull_request.body === "") {
+          core.setFailed("The body is empty, can't check");
+        } else {
+          const PRBody = context.payload.pull_request.body;
+          if (bodyContains && !rexify(bodyContains).test(PRBody)) {
+            core.setFailed(
+              "The body of the PR does not contain " + bodyContains
+            );
+          }
+          if (bodyDoesNotContain && rexify(bodyDoesNotContain).test(PRBody)) {
+            core.setFailed(
+              "The body of the PR should not contain " + bodyDoesNotContain
+            );
+          }
         }
-        if (bodyDoesNotContain && rexify(bodyDoesNotContain).test(PRBody)) {
+      }
+
+      if (context.payload.repository.private !== true) {
+        core.info("Checking diff contents");
+        const diffContains = core.getInput("diffContains");
+        const diffDoesNotContain = core.getInput("diffDoesNotContain");
+        const diff_url = context.payload.pull_request.diff_url;
+        core.info("Requesting " + diff_url);
+        const result = await github.request(diff_url);
+        core.info(result);
+        const files = parse(result.data);
+        core.exportVariable("files", files);
+        core.setOutput("files", files);
+        const filesChanged = +core.getInput("filesChanged");
+        if (filesChanged && files.length != filesChanged) {
           core.setFailed(
-            "The body of the PR should not contain " + bodyDoesNotContain
+            "You should change exactly " + filesChanged + " file(s)"
           );
         }
-      }
-    }
 
-    if (context.payload.repository.private !== true) {
-      core.info("Checking diff contents");
-      const diffContains = core.getInput("diffContains");
-      const diffDoesNotContain = core.getInput("diffDoesNotContain");
-      const diff_url = context.payload.pull_request.diff_url;
-      core.info("Requesting " + diff_url);
-      const result = await github.request(diff_url);
-      core.info(result);
-      const files = parse(result.data);
-      core.exportVariable("files", files);
-      core.setOutput("files", files);
-      const filesChanged = +core.getInput("filesChanged");
-      if (filesChanged && files.length != filesChanged) {
-        core.setFailed(
-          "You should change exactly " + filesChanged + " file(s)"
-        );
-      }
-
-      let changes = "";
-      let additions: number = 0;
-      files.forEach(function (file) {
-        additions += file.additions;
-        file.chunks.forEach(function (chunk: parse.Chunk) {
-          chunk.changes.forEach(function (change: any) {
-            if (change.add) {
-              changes += change.content;
-            }
+        let changes = "";
+        let additions: number = 0;
+        files.forEach(function (file) {
+          additions += file.additions;
+          file.chunks.forEach(function (chunk: parse.Chunk) {
+            chunk.changes.forEach(function (change: any) {
+              if (change.add) {
+                changes += change.content;
+              }
+            });
           });
         });
-      });
-      if (diffContains && !rexify(diffContains).test(changes)) {
-        core.setFailed("The added code does not contain " + diffContains);
+        if (diffContains && !rexify(diffContains).test(changes)) {
+          core.setFailed("The added code does not contain " + diffContains);
+        } else {
+          core.exportVariable("diff", changes);
+          core.setOutput("diff", changes);
+        }
+        if (diffDoesNotContain && rexify(diffDoesNotContain).test(changes)) {
+          core.setFailed(
+            "The added code should not contain " + diffDoesNotContain
+          );
+        }
+
+        core.info("Checking lines/files changed");
+        const linesChanged = +core.getInput("linesChanged");
+        if (linesChanged && additions != linesChanged) {
+          const this_msg =
+            "You should change exactly " +
+            linesChanged +
+            " lines(s) and you have changed " +
+            additions;
+          core.setFailed(this_msg);
+        }
       } else {
-        core.exportVariable("diff", changes);
-        core.setOutput("diff", changes);
-      }
-      if (diffDoesNotContain && rexify(diffDoesNotContain).test(changes)) {
-        core.setFailed(
-          "The added code should not contain " + diffDoesNotContain
+        core.warning(
+          "⚠️ I'm sorry, can't check diff in private repositories with the default token"
         );
       }
-
-      core.info("Checking lines/files changed");
-      const linesChanged = +core.getInput("linesChanged");
-      if (linesChanged && additions != linesChanged) {
-        const this_msg =
-          "You should change exactly " +
-          linesChanged +
-          " lines(s) and you have changed " +
-          additions;
-        core.setFailed(this_msg);
-      }
-    } else {
-      core.warning(
-        "⚠️ I'm sorry, can't check diff in private repositories with the default token"
-      );
     }
   } catch (error: any) {
     if (error.name === "HttpError") {
