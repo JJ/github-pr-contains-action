d7dfdec57e2ff95062ad71488bb9dbe4014fc4c4
JJ Merelo <jjmerelo@gmail.com>
2026-08-05 10:18:14 +0200

    Upgrading to ESM

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,7 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  transform: {
+    '^.+\\.ts$': ['ts-jest', { tsconfig: { module: 'commonjs', moduleResolution: 'node' } }],
+  },
 };
